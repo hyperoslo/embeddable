835f70c3c979af61fa1fa5e468251da8a3679606
Pål Andre Sundt <pal@rubynor.com>
2014-06-11 12:17:19 +0200

    Use #embeddable_from and #embeddable_name instead of assuming

--- lib/embeddable/view_helpers.rb
+++ lib/embeddable/view_helpers.rb
@@ -1,24 +1,27 @@
 module ViewHelpers
   def embed_video(embeddable, width, height)
-    attributes = { id: embeddable.video_id, width: width, height: height }
+    name = embeddable.embeddable_name.to_s
+    from = embeddable.embeddable_from
 
-    if embeddable.video_on_youtube?
+    attributes = { id: embeddable.send("#{name}_id"), width: width, height: height }
+
+    if embeddable.send("#{name}_on_youtube?")
       render 'embeddable/partials/youtube', attributes
-    elsif embeddable.video_on_vimeo?
+    elsif embeddable.send("#{name}_on_vimeo?")
       render 'embeddable/partials/vimeo', attributes
-    elsif embeddable.video_on_dailymotion?
+    elsif embeddable.send("#{name}_on_dailymotion?")
       render 'embeddable/partials/dailymotion', attributes
-    elsif embeddable.video_on_veoh?
+    elsif embeddable.send("#{name}_on_veoh?")
       render 'embeddable/partials/veoh', attributes
-    elsif embeddable.video_on_vippy?
+    elsif embeddable.send("#{name}_on_vippy?")
       render 'embeddable/partials/vippy', attributes
-    elsif embeddable.video_on_liveleak?
+    elsif embeddable.send("#{name}_on_liveleak?")
       # not supported
-      link_to embeddable.url, embeddable.url
-    elsif embeddable.video_on_brightcove?
+      link_to embeddable.send(from), embeddable.send(from)
+    elsif embeddable.send("#{name}_on_brightcove?")
       render 'embeddable/partials/brightcove', attributes
     else
-      link_to embeddable.url, embeddable.url
+      link_to embeddable.send(from), embeddable.send(from)
     end
   end
 end
